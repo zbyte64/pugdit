2ce4289df3c2a4d5d985f3107fcca5e2d1b7a97c
Jason Kraus <zbyte64@gmail.com>
2018-08-30 13:25:00 -0700

    use fragments for efficient caching

--- pugboat/src/components/CorePost.vue
+++ pugboat/src/components/CorePost.vue
@@ -10,13 +10,13 @@
               <avatar :user="post.signer" />
           </v-avatar>
           <v-spacer/>
-          <v-btn icon color="red lighten-2" @click.prevent="vote(-1)">
+          <v-btn icon :disabled="downVoted" color="red lighten-2" @click.prevent="vote(-1)">
             <v-icon>thumb_down</v-icon>
           </v-btn>
           <v-subheader color="grey">
               {{post.karma}}
           </v-subheader>
-            <v-btn icon color="blue lighten-2" @click.prevent="vote(1)">
+            <v-btn icon :disabled="upVoted" color="blue lighten-2" @click.prevent="vote(1)">
               <v-icon>thumb_up</v-icon>
             </v-btn>
           <v-spacer/>
@@ -32,6 +32,7 @@ import Avatar from './Avatar.vue'
 import sanitizeHtml from 'sanitize-html';
 import {getGraphId} from '../mailbox.js'
 import VOTE from '../graphql/Vote.gql'
+import POST from '../graphql/PostFragment.gql'
 import _ from 'lodash'
 
 export default {
@@ -45,16 +46,36 @@ export default {
   computed: {
     isImage() {
         return _.startsWith(this.post.file.contentType, 'image')
-    }
+    },
+    downVoted() {
+        let vote = this.$props.post.userVote
+        if (!vote) return false
+        return vote.karma == -1
+    },
+    upVoted() {
+        let vote = this.$props.post.userVote
+        if (!vote) return false
+        return vote.karma == 1
+    },
   },
   methods: {
       async vote(karma) {
-          await this.$apollo.mutate({
+          let response = await this.$apollo.mutate({
             mutation: VOTE,
             variables: {
                 post: getGraphId(this.post.id),
                 karma,
             }
+          })
+          console.log('vote response', response)
+          let vote = response.data.vote.vote
+          let voteUpdate = {
+              userVote: vote
+          }
+          this.$apollo.getClient().writeFragment({
+              //id: this.post.id,
+              fragment: POST,
+              data: _.assign({}, this.post, vote.post, voteUpdate)
           })
       },
       viewPost() {
